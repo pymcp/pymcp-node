d00b9d36134b40cbc5e2d3ded2f611217ff40c63
Mike Patterson <mike@pymcp.net>
2016-03-05 15:58:07 -0800

    Starting out

---
+++ app.js
@@ -0,0 +1,6 @@
+var http = require('http');
+var server = http.createServer(function(req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end("hello world2!\n");
+});
+server.listen(3000);
