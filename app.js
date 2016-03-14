0f6b58d7c0a47bfd23ea06dc12e917c0f161922b
Mike Patterson <mike@pymcp.net>
2016-03-13 20:08:42 -0700

    Mongodb integration

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 var app = require('express')();
-var http = require('http').Server(app)
+var http = require('http').Server(app);
+var mongodb = require('mongodb');
+
+mongodb.MongoClient.connect('mongodb://pymcp:jE71sQjC7S77iCGeEvWsEdL@localhost/pymcp', function(err, db) {
+  if (err) {
+    console.log('Unable to connect to the mongoDB server. Error:', err);
+  } else {
+    //HURRAY!! We are connected. :)
+    console.log('Connection established to', url);
+
+    // do some work here with the database.
+
+    //Close connection
+    db.close();
+  }
+})
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
