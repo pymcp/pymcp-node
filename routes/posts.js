b849ed80e60faa88511a773fa35390917a16908e
Mike Patterson <mike@pymcp.net>
2016-04-11 19:15:55 -0700

    Return a 500 on error

--- routes/posts.js
+++ routes/posts.js
@@ -7,6 +7,7 @@ router.put('/', function(req, res, next) {
   var post = new mongo.Post({subject: 'test', message: 'testing'});
   post.save(function(err) {
     if (err) {
+      res.status(500);
       res.json({status: 'error', message: err}); 
     } else {
       res.json({status: 'ok'});
