4498a3bf0a70c4b9a355e80cb92561f5449e8940
Mike Patterson <mike@pymcp.net>
2016-04-11 20:53:33 -0700

    Added method to get posts

--- routes/posts.js
+++ routes/posts.js
@@ -3,6 +3,17 @@ var mongo = require('../models/mongo');
 
 var router = express.Router();
 
+router.get('/', function(req, res, next) {
+  mongo.Post.find(function(err, posts) {
+    if (err) {
+      res.status(500);
+      res.json({status: 'error', message: error});
+    } else {
+      res.json(posts);
+    }
+  });
+});
+
 router.put('/', function(req, res, next) {
   var post = new mongo.Post({subject: req.body.subject, message: req.body.message});
   post.save(function(err) {
